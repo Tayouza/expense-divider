ff90de59732a3f7b7e84dbabd83e98ee6db2ac26
tayouza <tayouzadev@gmail.com>
2026-03-03 18:35:23 -0300

    feat: update laravel to 12 and php to 8.4

---
+++ rector.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use RectorLaravel\Set\LaravelLevelSetList;
+use RectorLaravel\Set\Packages\Livewire\LivewireSetList;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/app',
+        __DIR__ . '/config',
+        __DIR__ . '/database',
+        __DIR__ . '/routes',
+        __DIR__ . '/tests',
+    ])
+    ->withPhpSets()
+    ->withSets([
+        LaravelLevelSetList::UP_TO_LARAVEL_120,
+        LivewireSetList::LIVEWIRE_30,
+    ])
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        typeDeclarations: true,
+    )
+    ->withComposerBased(
+        phpunit: true,
+    );
